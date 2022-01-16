9bd2d1ee0043e28f712b71b9d263be8a7f2b0f7b
Lazaro Sanchez <lazarosanchezjs@gmail.com>
2022-01-15 22:41:23 -0300

    bug fix

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,7 +10,7 @@ export default function Search({setNames}) {
 
     const handleChange = ()=>{
         getPokemonNames(input.current.value).then(res => {
-            setNames(res)
+            setNames(res.results ? res.results : res)
         })
     }
 
