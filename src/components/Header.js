71a25651bc0f67deb45b7abeec465ef87fc25b2d
Lazaro Sanchez <lazarosanchezjs@gmail.com>
2022-01-27 23:35:12 -0300

    fix header style 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,15 @@ import Search from './Search'
 import { useContext } from "react"
 import pokemonList from "../context/pokemonList"
 import { Route, Routes} from 'react-router-dom'
+import pokefy from '../images/POKEFY.png'
 
 export default function Header() {
     const [names, setNames] = useContext(pokemonList)
 
     return (
     <header className="header">
-        <h1 className='title'>POK<br/>EFY</h1>
+        {/* <h1 className='title'>POK<br/>EFY</h1> */}
+        <img className='pokefy' src={pokefy} />
         <Routes><Route path='/' element={<Search names={names} setNames={setNames}/>}/></Routes>
     </header>
     )
