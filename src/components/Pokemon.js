17625092fd22303940805c9164ea2df52822a903
Lazaro Sanchez <lazarosanchezjs@gmail.com>
2022-02-05 00:36:22 -0300

    add pckg

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,8 +1,6 @@
 import './Pokemon.scss'
 import { useEffect, useState } from 'react'
 import getSinglePokemon from '../services/getSinglePokemon'
-import { Link } from 'react-router-dom'
-import Spinner from './Spinner'
 import getPokemonFlavor from '../services/getPokemonFlavor'
 
 export default function Pokemon({poke,name,url}) {
@@ -15,27 +13,40 @@ export default function Pokemon({poke,name,url}) {
         else setInfo(poke)
     },[])
 
-    // useEffect(()=>{
-    //     console.log(flavour)
-    // },[flavour])
-
     const openCard = ()=>{ 
         //click handler, text flavour fetching
-        getPokemonFlavor(info.species.url)
+        if (flavour) setFlavour(null)
+        else {
+            getPokemonFlavor(info.species.url)
             .then(res => {
-                setFlavour(res)
+                setFlavour(res[0])
+                // setFlavour(res)
             })
+        }
     }
+    // `/pokemon/${name || info.name}`
+    // https://www.npmjs.com/package/react-easy-swipe
 
-    return <Link to={`/pokemon/${name || info.name}`}>
-    {info ? 
-        <div className="each-poke" onClick={openCard}>
-            {info.sprites ? <img className="sprite" src={info.sprites.front_default} /> : ''}
-            <p className="poke__name">{name || info.name}</p>   
-            <strong> {'>>'} </strong>
-        </div>
-        : <Spinner />}
-    </Link>
+    if (!flavour) {
+        // return the closed card
+        return <>
+        {info ? 
+            <div className="each-poke" onClick={openCard}>
+                {info.sprites ? <img className="sprite" src={info.sprites.front_default} /> : ''}
+                <p className="poke__name">{name || info.name}</p>   
+                <strong> {'>>'} </strong>
+            </div>
+            : ''}</>
+    } else {
+        // return the open card
+        return <> 
+            <div className="each-poke active" onClick={openCard}>
+                <img className="sprite active" src={info.sprites.front_default}/>
+                <p className='flavor'>{flavour.flavor_text}</p>
+            </div>
+            </>
+
+    }
 
 }
 
